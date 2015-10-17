89486e90041674e2f2f9a2e522e0479522835c1b
Liam Morley <liam@carpeliam.com>
2015-10-17 15:35:33 -0700

    FIXME marker for future DnD testing

--- spec/client/piece_spec.js
+++ spec/client/piece_spec.js
@@ -1,7 +1,20 @@
 import '../client_helper';
 import React from 'react';
 import TestUtils from 'react/lib/ReactTestUtils';
+// import TestBackend from 'react-dnd-test-backend';
+// import { DragDropContext } from 'react-dnd';
 import Piece from '../../client/piece';
+
+// TODO figure out how to test dnd w/React 0.14
+// function wrapInTestContext(DecoratedComponent) {
+//   return DragDropContext(TestBackend)(
+//     class TestContextContainer extends React.Component {
+//       render() {
+//         return <DecoratedComponent {...this.props} />;
+//       }
+//     }
+//   );
+// }
 
 describe('Piece', () => {
   const OrigPiece = Piece.DecoratedComponent;
@@ -12,6 +25,12 @@ describe('Piece', () => {
     renderer.render(<OrigPiece pieceType={props.pieceType || 1} color={props.color || 1} connectDragSource={props.connectDragSource || identity} isDragging={props.isDragging || false} />);
     return renderer.getRenderOutput();
   }
+
+  // TODO figure out how to test dnd w/React 0.14
+  // function getRenderedContext(Context, props = {}) {
+  //   renderer.render(<Context pieceType={props.pieceType || 1} color={props.color || 1} />);
+  //   return renderer.getRenderOutput();
+  // }
 
   it('renders the appropriate piece type', () => {
     var result = getRenderedResult();
@@ -32,4 +51,17 @@ describe('Piece', () => {
     var result = getRenderedResult({isDragging: true});
     expect(result.props.style.opacity).toEqual(0.5);
   });
+
+  // TODO figure out how to test dnd w/React 0.14
+  // it('can be dragged if a drag source allows it', () => {
+  //   const PieceContext = wrapInTestContext(Piece);
+  //   const root = getRenderedContext(PieceContext);
+  //   // const root = TestUtils.renderIntoDocument(<PieceContext pieceType={1} color={1} />);
+
+  //   // const backend = root.getManager().getBackend();
+  //   debugger;
+  // //   var result = getRenderedResult({connectDragSource: () => {
+  // //     canDrag() { return true; }
+  // //   }})
+  // });
 });
