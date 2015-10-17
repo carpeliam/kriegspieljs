e34b224846c0e517899949ec3fc3cc9eee27ab78
Liam Morley <liam@carpeliam.com>
2015-10-17 15:34:01 -0700

    add additional testing around Piece

--- spec/client/piece_spec.js
+++ spec/client/piece_spec.js
@@ -6,14 +6,30 @@ import Piece from '../../client/piece';
 describe('Piece', () => {
   const OrigPiece = Piece.DecoratedComponent;
   const identity = el => el;
-  var component;
   var renderer = TestUtils.createRenderer();
-  beforeEach(() => {
-    component = TestUtils.renderIntoDocument(<OrigPiece pieceType={1} color={1} connectDragSource={identity} isDragging={false} />);
-  });
+
+  function getRenderedResult(props = {}) {
+    renderer.render(<OrigPiece pieceType={props.pieceType || 1} color={props.color || 1} connectDragSource={props.connectDragSource || identity} isDragging={props.isDragging || false} />);
+    return renderer.getRenderOutput();
+  }
+
   it('renders the appropriate piece type', () => {
-    renderer.render(<OrigPiece pieceType={1} color={1} connectDragSource={identity} isDragging={false} />);
-    var result = renderer.getRenderOutput();
+    var result = getRenderedResult();
     expect(result.props.children).toEqual('♙');
+  });
+  
+  it('renders the appropriate color', () => {
+    var result = getRenderedResult({color: -1});
+    expect(result.props.children).toEqual('♟');
+  });
+
+  it('is opaque if not currently being dragged', () => {
+    var result = getRenderedResult({isDragging: false});
+    expect(result.props.style.opacity).toEqual(1);
+  });
+
+  it('is 50% transparent if currently being dragged', () => {
+    var result = getRenderedResult({isDragging: true});
+    expect(result.props.style.opacity).toEqual(0.5);
   });
 });
