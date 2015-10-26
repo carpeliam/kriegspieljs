da0037973031d33dfbcfea6139d9012b8ae7c3c5
Liam Morley <liam@carpeliam.com>
2015-10-25 22:33:27 -0700

    Test showing/hiding a username modal prompt
    Currently this is using `ReactTestUtils.findRenderedComponentWithType` and then introspecting the `show` prop on the modal to determine if it's visible. I'd preferably like to use bootstrap classes, but `ReactTestUtils.findRenderedDOMComponentWithClass` doesn't work because the modal is using a [portal](https://github.com/tajo/react-portal) to render the modal outside of the render tree. I'd like to come back to this at some point and see if I can rewrite the tests to avoid introspecting on Modal props.

--- spec/client/username-prompter_spec.js
+++ spec/client/username-prompter_spec.js
@@ -1,11 +1,28 @@
 import '../client_helper';
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import { Modal } from 'react-bootstrap';
 
 import UserNamePrompter from '../../client/username-prompter';
 
 describe('UserNamePrompter', () => {
-  it('is visible if the username is null');
+  const onEnter = jasmine.createSpy('onEnter');
+  function createUserNamePrompter(props = {}) {
+    return ReactTestUtils.renderIntoDocument(<UserNamePrompter
+      user={props.user || null} onEnter={props.onEnter || onEnter} />);
+  }
+  it('is visible if the username is undefined', () => {
+    const component = createUserNamePrompter({user: undefined});
+    // TODO test via class name
+    const modal = ReactTestUtils.findRenderedComponentWithType(component, Modal);
+    expect(modal.props.show).toBe(true);
+  });
 
-  it('is hidden if the username is present');
+  it('is hidden if the username is present', () => {
+    const component = createUserNamePrompter({user: 'jim'});
+    const modal = ReactTestUtils.findRenderedComponentWithType(component, Modal);
+    expect(modal.props.show).toBe(false);
+  });
 
   describe('when the button is clicked', () => {
     it('calls the signin call back with the username');
