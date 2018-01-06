99d374b8f2bd6cd6ac70b2f1b03037edab709a25
Liam Morley <liam@carpeliam.com>
2018-01-06 15:14:24 -0500

    Utilize js-cookie built-in JSON support

--- client/cookie-monster.js
+++ client/cookie-monster.js
@@ -4,9 +4,9 @@ import shortid from 'shortid';
 const COOKIE_NAME = 'kriegspiel.user';
 
 export function fetchUser() {
-  const cookie = Cookie.get(COOKIE_NAME);
+  const cookie = Cookie.getJSON(COOKIE_NAME);
   if (cookie) {
-    return JSON.parse(cookie);
+    return cookie;
   } else {
     const user = { id: shortid.generate() };
     setUser(user);
@@ -15,5 +15,5 @@ export function fetchUser() {
 }
 
 export function setUser(user) {
-  Cookie.set(COOKIE_NAME, JSON.stringify(user));
+  Cookie.set(COOKIE_NAME, user);
 }
--- spec/client/cookie-monster_spec.js
+++ spec/client/cookie-monster_spec.js
@@ -4,26 +4,30 @@ import { fetchUser, setUser } from '../../client/cookie-monster';
 
 describe('cookieMonster', () => {
   const user = { id: 'abc123', name: 'margaret' };
+  beforeEach(() => {
+    spyOn(Cookie, 'getJSON');
+    spyOn(Cookie, 'set');
+  });
   describe('#fetchUser', () => {
     it('retrieves and parses a user from a cookie when present', () => {
-      spyOn(Cookie, 'get').and.returnValue(JSON.stringify(user));
+      Cookie.getJSON.and.returnValue(user);
       expect(fetchUser()).toEqual(user);
-      expect(Cookie.get).toHaveBeenCalledWith('kriegspiel.user');
+      expect(Cookie.getJSON).toHaveBeenCalledWith('kriegspiel.user');
     });
 
-    it('returns a new ID when a cookie is not present', () => {
+    it('returns a new ID and sets the user when a cookie is not present', () => {
+      Cookie.getJSON.and.returnValue(undefined);
       spyOn(shortid, 'generate').and.returnValue('abc123');
-      spyOn(Cookie, 'get').and.returnValue(undefined);
       expect(fetchUser()).toEqual({ id: 'abc123' });
-      expect(Cookie.get).toHaveBeenCalledWith('kriegspiel.user');
+      expect(Cookie.getJSON).toHaveBeenCalledWith('kriegspiel.user');
+      expect(Cookie.set).toHaveBeenCalled();
     });
   });
 
   describe('#setUser', () => {
     it('sets a stringified version of the user', () => {
-      spyOn(Cookie, 'set');
       setUser(user);
-      expect(Cookie.set).toHaveBeenCalledWith('kriegspiel.user', JSON.stringify(user));
+      expect(Cookie.set).toHaveBeenCalledWith('kriegspiel.user', user);
     });
   });
 });
