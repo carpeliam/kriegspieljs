544ae9641a2a7fb6885a22c0819a8716fb0d0953
Liam Morley <liam@carpeliam.com>
2018-01-19 21:34:57 -0500

    Place black at the bottom if black is seated

--- client/board.js
+++ client/board.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import Square from './square';
 
-export function Board(props) {
+const ascending = [0, 1, 2, 3, 4, 5, 6, 7];
+const descending = [7, 6, 5, 4, 3, 2, 1, 0];
+function Board({ observingColor }) {
+  const rows = (observingColor === 'black') ? ascending : descending;
+  const cols = (observingColor === 'black') ? descending : ascending;
   return (
     <div className="board">
-      {[7, 6, 5, 4, 3, 2, 1, 0].map((y) =>
-        [0, 1, 2, 3, 4, 5, 6, 7].map((x) =>
-          <Square x={x} y={y} />
-        )
-      )}
+      {rows.map(y => cols.map(x => <Square x={x} y={y} />))}
     </div>
   );
 }
 
-export default DragDropContext(HTML5Backend)(Board);
+Board.propTypes = {
+  observingColor: PropTypes.oneOf(['white', 'black']).isRequired,
+};
+
+export const BoardDragDropContext = DragDropContext(HTML5Backend)(Board);
+
+function mapStateToProps({ user, game: { players } }) {
+  const observingColor = (players.black && players.black.id === user.id) ? 'black' : 'white';
+  return { observingColor };
+}
+
+export default connect(mapStateToProps)(BoardDragDropContext);
--- spec/client/board_spec.js
+++ spec/client/board_spec.js
@@ -1,18 +1,61 @@
 import React from 'react';
+import { createStore } from 'redux';
 import { shallow } from 'enzyme';
-import BoardDragDropContext from '../../client/board';
+import BoardContainer, { BoardDragDropContext } from '../../client/board';
 import Square from '../../client/square';
 
 const Board = BoardDragDropContext.DecoratedComponent;
 
 describe('Board', () => {
   it('renders 64 squares', () => {
-    const board = shallow(<Board />);
+    const board = shallow(<Board observingColor="white" />);
     const squares = board.find(Square);
     expect(squares.length).toEqual(64);
-    expect(squares.at(0)).toHaveProp('x', 0);
-    expect(squares.at(0)).toHaveProp('y', 7);
-    expect(squares.at(63)).toHaveProp('x', 7);
-    expect(squares.at(63)).toHaveProp('y', 0);
+  });
+
+  describe('when the current user is either playing as white or not playing', () => {
+    it('shows the A1 square at the bottom left of the board', () => {
+      const board = shallow(<Board observingColor="white" />);
+      const squares = board.find(Square);
+      expect(squares.at(0)).toHaveProp('x', 0);
+      expect(squares.at(0)).toHaveProp('y', 7);
+      expect(squares.at(63)).toHaveProp('x', 7);
+      expect(squares.at(63)).toHaveProp('y', 0);
+    });
+  });
+
+  describe('when the current user is playing as black', () => {
+    it('shows the H8 square at the bottom left of the board', () => {
+      const board = shallow(<Board observingColor="black" />);
+      const squares = board.find(Square);
+      expect(squares.at(0)).toHaveProp('x', 7);
+      expect(squares.at(0)).toHaveProp('y', 0);
+      expect(squares.at(63)).toHaveProp('x', 0);
+      expect(squares.at(63)).toHaveProp('y', 7);
+    });
+  });
+});
+
+describe('Board container', () => {
+  let container;
+  let store;
+  const players = {
+    white: { id: 'abc123', name: 'Bobby' },
+    black: { id: '123abc', name: 'Gary' },
+  };
+  it('passes the observing color as white if white is the current user', () => {
+    store = createStore(state => state, { user: players.white, game: { players } });
+    container = shallow(<BoardContainer store={store} />);
+    expect(container.find(BoardDragDropContext)).toHaveProp('observingColor', 'white');
+  });
+  it('passes the observing color as black if black is the current user', () => {
+    store = createStore(state => state, { user: players.black, game: { players } });
+    container = shallow(<BoardContainer store={store} />);
+    expect(container.find(BoardDragDropContext)).toHaveProp('observingColor', 'black');
+  });
+  it('does not pass an observing color if the current user is not playing', () => {
+    store = createStore(state => state, { user: { id: 'guest' }, game: { players } });
+    container = shallow(<BoardContainer store={store} />);
+    expect(container.find(BoardDragDropContext)).toHaveProp('observingColor', 'white');
   });
 });
